c1dfb7ef9088d1b07dfc0ebe0cba8aea531bc197
c-daniel14 <cojocarugeani@yahoo.com>
2018-07-29 17:19:10 +0100

    test

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ const getJokes = (e) => {
     xhr.onload = () => {
         if(this.status === 200) {
             const response = JSON.parse(this.responseText);
-            console.log(response);
+            let output = '';
+
+            if(response.type === 'success') {
+                response.value.forEach( joke => {
+                    output += `<li>${joke.joke}</li>`;    
+                });
+            } else {
+                output += `<li>Something went wrong</li>`;
+            }
+
+            // Display joke
+            document.querySelector('.jokes').innerHTML = output;
         } 
     }
 
